ee99d483ac5d64609ad81993bb1ef73018efb5a5
HangenYuu <pnhung1703@gmail.com>
2023-07-26 06:46:35 +0000

    finished onnx experiment

--- inference_onnx.py
+++ inference_onnx.py
@@ -1,18 +1,13 @@
 from PIL import Image
 import torch
 import torchvision.transforms as T
-from food101classifier import Food101Classifier
 from utils import timing
+import onnxruntime as ort
 
-class Food101Predictor:
+class Food101ONNXPredictor:
     def __init__(self, model_path) -> None:
-        self.model_path = model_path
-        # if torch.cuda.is_available():
-        #     self.model = Food101Classifier.load_from_checkpoint(model_path) # type: ignore
-        # else:
-        self.model = Food101Classifier.load_from_checkpoint(model_path, map_location='cpu')
-        self.model.eval()
-        self.model.freeze()
+        self.ort_session = ort.InferenceSession(model_path)
+        self.input_name = self.ort_session.get_inputs()[0].name
         self.transform = T.Compose([
                     T.Resize(248),
                     T.CenterCrop(224),
@@ -22,30 +17,24 @@ class Food101Predictor:
         self.softmax = torch.nn.Softmax(dim=0)
         with open('labels.txt', 'r') as f:
             self.idx_to_label = [s.strip() for s in f.readlines()]
+
     @timing
     def predict(self, input_image):
         input_tensor = self.transform(input_image)
-        input_batch = input_tensor.unsqueeze(0)
-
-        # if torch.cuda.is_available():
-        #     print('CUDA device detected. Switched to CUDA device for faster inference')
-        #     input_batch = input_batch.to('cuda')
-        # else:
-        #     print('Using CPU for inference. Will be slower')
-        
-        with torch.inference_mode():
-            output = self.model(input_batch)
-        
-        probabilities = self.softmax(output[0])
+        input_batch = input_tensor.unsqueeze(0).numpy()
+        ort_inputs = {self.input_name: input_batch}
+        ort_outs = torch.Tensor(self.ort_session.run(None, ort_inputs)[0])
+        probabilities = self.softmax(ort_outs[0])
         top5_prob, top5_catid = torch.topk(probabilities, 5)
         # Label:probability
         result = {self.idx_to_label[int(idx)]:val.item() for val, idx in zip(top5_prob.cpu(), top5_catid.cpu())}
         return result
-    
+
+
 if __name__ == "__main__":
     image_path = "pablo-pacheco-D3Mag4BKqns-unsplash.jpg"
     pil_image = Image.open(image_path)
-    predictor = Food101Predictor("models/levit_256/checkpoints-v1.ckpt")
+    predictor = Food101ONNXPredictor("models/levit_256/onnx/checkpoints-v1.onnx")
     print(predictor.predict(pil_image))
     # for _ in range(20):
     #     predictor.predict(pil_image)
